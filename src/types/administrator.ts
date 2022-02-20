2c0f11a3317c0e650f908e974dec8b2d240a8a18
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-20 16:50:05 +0900

    Administratorクラス作成

---
+++ src/types/administrator.ts
@@ -0,0 +1,40 @@
+export class Administrator {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _mailAddress: string,
+    private _password: number
+  ) {}
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public get mailAddress(): string {
+    return this._mailAddress;
+  }
+
+  public set mailAddress(mailAddress: string) {
+    this._mailAddress = mailAddress;
+  }
+
+  public get password(): number {
+    return this._password;
+  }
+
+  public set password(password: number) {
+    this._password = password;
+  }
+}
